ca5ff679cc95f46b92c62fc5204ab3a7ef284895
BazFouFou <105197334+BazFouFou@users.noreply.github.com>
2024-05-31 12:59:38 +0200

    Create reservation.php

---
+++ reservation.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sportifydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT date_rdv FROM prise_de_rendez_vous WHERE statut_rdv = 1";
+$result = $conn->query($sql);
+
+$booked_slots = [];
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $date_time = new DateTime($row['date_rdv']);
+        $day = strtolower($date_time->format('D'));
+        $time = str_replace(':', '-', $date_time->format('H:i'));
+        $slot = $day . '-' . $time;
+        $booked_slots[] = $slot;
+    }
+}
+
+$conn->close();
+
+header('Content-Type: text/plain');
+echo implode(',', $booked_slots);
+?>
